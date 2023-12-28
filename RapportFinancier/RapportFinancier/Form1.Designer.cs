5d7c7e7cdc1950e0245988ff6243360663023b02
Rayan Hasbellaoui <rayan.hasbellaoui@gmail.com>
2023-12-28 16:34:54 -0500

    project init

---
+++ RapportFinancier/RapportFinancier/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace RapportFinancier
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            buttonGenerate = new Button();
+            SuspendLayout();
+            // 
+            // buttonGenerate
+            // 
+            buttonGenerate.Location = new Point(153, 107);
+            buttonGenerate.Name = "buttonGenerate";
+            buttonGenerate.Size = new Size(120, 36);
+            buttonGenerate.TabIndex = 0;
+            buttonGenerate.Text = "Generate";
+            buttonGenerate.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(442, 247);
+            Controls.Add(buttonGenerate);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button buttonGenerate;
+    }
+}
